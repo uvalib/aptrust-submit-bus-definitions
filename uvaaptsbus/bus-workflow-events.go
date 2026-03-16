60b3597dab4a1c0818656e313bbba1ec3352f9c3
Dave G <dpg3k@virginia.edu>
2026-03-16 14:16:57 -0400

    Fix misspelling

--- uvaaptsbus/bus-workflow-events.go
+++ uvaaptsbus/bus-workflow-events.go
@@ -27,12 +27,12 @@ var EventSubmissionComplete = "workflow.submission.complete"     // submission c
 var EventSubmissionIncomplete = "workflow.submission.incomplete" // submission incomplete
 //var EventSubmissionRejected = "workflow.submission.rejected"           // submission rejected
 
-var EventBagInitiate = "workflow.bag.initiate"  // bag initiate
-var EventBagBuilt = "workflow.bag.built"        // bag has been built
-var EventBagSubmit = "workflow.bag.submit"      // bag submit (to APT)
-var EventBagSubmitted = "workflow.bag.submited" // bag submitted (to APT)
-var EventBagAccepted = "workflow.bag.accepted"  // bag accepted (by APT)
-var EventBagRejected = "workflow.bag.rejected"  // bag rejected (by APT)
+var EventBagInitiate = "workflow.bag.initiate"   // bag initiate
+var EventBagBuilt = "workflow.bag.built"         // bag has been built
+var EventBagSubmit = "workflow.bag.submit"       // bag submit (to APT)
+var EventBagSubmitted = "workflow.bag.submitted" // bag submitted (to APT)
+var EventBagAccepted = "workflow.bag.accepted"   // bag accepted (by APT)
+var EventBagRejected = "workflow.bag.rejected"   // bag rejected (by APT)
 
 //
 // corresponding schema for these events
