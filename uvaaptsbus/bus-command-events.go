5517dd05fe3089b857102ca9373fbd5e02e6063e
Dave G <dpg3k@virginia.edu>
2026-04-16 10:14:30 -0400

    Additional command events for purge

--- uvaaptsbus/bus-command-events.go
+++ uvaaptsbus/bus-command-events.go
@@ -9,9 +9,11 @@ package uvaaptsbus
 //
 
 var EventCommandSubmissionInitiate = "command.submission.initiate" // command to reprocess a submission
+var EventCommandSubmissionPurge = "command.submission.purge"       // command to purge a submission's assets
 var EventCommandBagInitiate = "command.bag.initiate"               // command to reprocess a bag
 var EventCommandBagSubmit = "command.bag.submit"                   // command to submit a bag (to APT)
 var EventCommandBagStatus = "command.bag.status"                   // command to request a bag status (from APT)
+var EventCommandBagPurge = "command.bag.purge"                     // command to purge a bag's assets
 
 //
 // end of file
